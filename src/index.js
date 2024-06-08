5051cae7b0e219964c377517cc424052329caa6c
senay <senaykt@gmail.com>
2024-06-08 09:16:57 -0700

    Added axios

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function handleEvent(event) {
+  event.preventDefault()
+  let searchFormInputElement = document.querySelector('#search-form-input')
+  let cityElement = document.querySelector('#city')
+  cityElement.innerHTML = searchFormInputElement.value
+}
+
+let searchFormElement = document.querySelector('#search-form')
+searchFormElement.addEventListener('submit', handleEvent)
