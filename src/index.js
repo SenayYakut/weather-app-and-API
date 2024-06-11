74d9cdd75bdce9db9ebc6d708c5abef8c92dca56
senay <senaykt@gmail.com>
2024-06-10 18:39:03 -0700

    Added search functionality using API's

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+function refreshWeather(response) {
+  let cityElement = document.querySelector('#city')
+  cityElement.innerHTML = response.data.city
+  let temperatureElement = document.querySelector('#temperature')
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current)
+}
+
+function searchCity(city) {
+  let apiKey = '7e3b4415c5b18tf300a062dfeo8d69f7'
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
+  axios.get(apiUrl).then(refreshWeather)
+}
 function handleEvent(event) {
   event.preventDefault()
   let searchFormInputElement = document.querySelector('#search-form-input')
-  let cityElement = document.querySelector('#city')
-  cityElement.innerHTML = searchFormInputElement.value
+
+  searchCity(searchFormInputElement.value)
 }
 
 let searchFormElement = document.querySelector('#search-form')
 searchFormElement.addEventListener('submit', handleEvent)
+
+searchCity('Istanbul')
