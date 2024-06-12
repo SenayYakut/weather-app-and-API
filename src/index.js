3ed06d256bafb358f53baa36f5f817661f052050
senay <senaykt@gmail.com>
2024-06-11 19:40:57 -0700

    Added real data information usin API's

--- src/index.js
+++ src/index.js
@@ -4,13 +4,78 @@ function refreshWeather(response) {
   let cloudElement = document.querySelector('#cloud')
   let humidityElement = document.querySelector('#humidity')
   let windElement = document.querySelector('#wind')
+  let dateElement = document.querySelector('#date')
+  let date = new Date(response.data.time * 1000)
+  let iconElement = document.querySelector('#icon')
+  let icon_url = response.data.condition.icon_url
   console.log(response.data)
+
+  iconElement.innerHTML = `<img src="${icon_url}" class="weather-app-icon"/>`
+  dateElement.innerHTML = formatDate(date)
   windElement.innerHTML = `${response.data.wind.speed}km/h`
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`
 
   cloudElement.innerHTML = response.data.condition.description
   cityElement.innerHTML = response.data.city
   temperatureElement.innerHTML = Math.round(response.data.temperature.current)
+}
+function formatDate(date) {
+  let minutes = date.getMinutes()
+  let hours = date.getHours()
+  let days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
+  let day = days[date.getDay()]
+
+  return `${day} ${hours}:${minutes}`
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes()
+  let hours = date.getHours()
+  let days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
+  let day = days[date.getDay()]
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+
+  return `${day} ${hours}:${minutes}`
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes()
+  let hours = date.getHours()
+  let days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
+  let day = days[date.getDay()]
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+
+  return `${day} ${hours}:${minutes}`
 }
 
 function searchCity(city) {
